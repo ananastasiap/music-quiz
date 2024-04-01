d9f0c03e5aa880854fcee8c14da97cd26f892985
ananastasiap <anestheziia@gmail.com>
2024-04-01 21:49:41 +0300

    init: proj music-quiz )

---
+++ app/quiz/page.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { quiz } from "../composerData";
+
+const Page = () => {
+  return (
+    <div className="container">
+      <h1>Композитор</h1>
+      <div>
+        <h2>
+          Вопрос: 1<span>/18</span>
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
