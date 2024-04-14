d63a9d8547a385cfe106dac5efb780e40ac56025
ananastasiap <anestheziia@gmail.com>
2024-04-14 21:52:49 +0300

    feat: style games page

--- app/games/page.jsx
+++ app/games/page.jsx
@@ -1,14 +1,24 @@
 "use client";
 import Link from "next/link";
+import styles from "./page.module.scss";
+import Header from "../components/Header";
 
 const MenuGames = () => {
   return (
-    <div>
-      <h1>Выберите игру:</h1>
-      <Link href="../game1/">Игра 1</Link>
-      <br />
-      <Link href="../game2/">Игра 2</Link>
-    </div>
+    <>
+      <Header />
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <Link href="../game1/" className={styles.btnGame}>
+            Игра 1
+          </Link>
+          <br />
+          <Link href="../game2/" className={styles.btnGame}>
+            Игра 2
+          </Link>
+        </div>
+      </main>
+    </>
   );
 };
 
